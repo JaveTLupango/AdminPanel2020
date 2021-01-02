d8195a1f3576848e10ff937b173206a3f15adc5c
Jave Lupango <jave.lupango@rdpawnshop.com>
2021-01-02 13:26:56 +0800

    footer

--- view/partial/admin.footer.php
+++ view/partial/admin.footer.php
@@ -1,7 +1,7 @@
 <?php
 
 echo '<footer class="main-footer">
-    <strong>Copyright &copy; '. date('Y') .' <a href="https://javelupango.com/">JAVE</a>.</strong>
+    <strong>Copyright &copy; '. date('Y') .' Powered BY: <a href="https://codelife.javelupango.com/">CodeLife</a> Created BY: <a href="https://javelupango.com/">Jave</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>'.$logsURL.'</b>
